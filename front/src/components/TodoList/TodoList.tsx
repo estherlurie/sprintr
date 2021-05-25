c1db5eed912508cb349831bf260cd21e0b9e2ec0
Danielle Lurie <dlurie24@gmail.com>
2021-05-24 22:12:29 -0700

    begin dev

---
+++ front/src/components/TodoList/TodoList.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { TodoListItem } from "..";
+import Card from "react-bootstrap/esm/Card";
+
+interface Props {
+  todos: Todo[];
+  toggleTodo: ToggleTodo;
+}
+
+export const TodoList: React.FC<Props> = ({ todos, toggleTodo }) => {
+  return (
+    <>
+      <ul>
+        {todos.map((todo) => {
+          return (
+            <TodoListItem key={todo.text} todo={todo} toggleTodo={toggleTodo} />
+          );
+        })}
+      </ul>
+    </>
+  );
+};
---
+++ front/src/components/TodoList/TodoListItem.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+
+interface Props {
+  todo: Todo;
+  toggleTodo: ToggleTodo;
+}
+
+export const TodoListItem: React.FC<Props> = ({ todo, toggleTodo }) => {
+  return (
+    <>
+      <li>
+        <label
+          style={{ textDecoration: todo.complete ? "line-through" : undefined }}
+        >
+          <input
+            type="checkbox"
+            checked={todo.complete}
+            onClick={() => {
+              toggleTodo(todo);
+            }}
+          />{" "}
+          <div>
+            {todo.text}
+            {" | Energy: "}
+            {todo.energy}
+            {" | Started: "}
+            {todo.startDate.getMonth() +
+              1 +
+              "/" +
+              todo.startDate.getUTCDate() +
+              "/" +
+              todo.startDate.getUTCFullYear()}
+          </div>
+        </label>
+      </li>
+    </>
+  );
+};
